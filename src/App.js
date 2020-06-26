4fecfdf7c3116740a2bb9e3e4d756b5b143edb7b
Jesse McKinney <jessemckinney513@gmail.com>
2020-06-26 17:20:06 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MyCarousel from "../src/MyCarousel/MyCarousel";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <MyCarousel />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
